da7196ba3afddd62da42ecad3cd242935b35a71f
William Moses <w.moses1@newcastle.ac.uk>
2021-05-28 15:11:43 +0100

    Added Diluc Overview

--- app/src/main/java/com/example/genshinapp/MondstadtFragment.java
+++ app/src/main/java/com/example/genshinapp/MondstadtFragment.java
@@ -71,6 +71,14 @@ public class MondstadtFragment extends Fragment {
             }
         });
 
+        binding.diluc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MondstadtFragment.this)
+                        .navigate(R.id.action_Mondstadt_to_dilucFragment);
+            }
+        });
+
     }
 
     @Override
