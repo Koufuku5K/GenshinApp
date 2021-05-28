4589a0410a3afde88f6b6365c5868072f57717fe
William Moses <w.moses1@newcastle.ac.uk>
2021-05-28 12:17:04 +0100

    Added Lisa Overview

--- app/src/main/java/com/example/genshinapp/MondstadtFragment.java
+++ app/src/main/java/com/example/genshinapp/MondstadtFragment.java
@@ -46,6 +46,14 @@ public class MondstadtFragment extends Fragment {
             }
         });
 
+        binding.lisa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MondstadtFragment.this)
+                        .navigate(R.id.action_Mondstadt_to_lisaFragment);
+            }
+        });
+
     }
 
     @Override
