fbcb5c0359aee0d5181eb6bb6071abf1931b61a3
William Moses <w.moses1@newcastle.ac.uk>
2021-05-29 13:25:43 +0100

    Added Kaeya overview and new mondstadt fragment

--- app/src/main/java/com/example/genshinapp/MondstadtFragment.java
+++ app/src/main/java/com/example/genshinapp/MondstadtFragment.java
@@ -121,6 +121,15 @@ public class MondstadtFragment extends Fragment {
             }
         });
 
+        //proceed to kaeya fragment when button is clicked
+        binding.kaeya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MondstadtFragment.this)
+                        .navigate(R.id.action_Mondstadt_to_kaeyaFragment);
+            }
+        });
+
     }
 
     @Override
