2e725ac9417d81c4f53039db7703eb4faa2145b4
William Moses <w.moses1@newcastle.ac.uk>
2021-06-10 12:39:07 +0100

    Added Bennett Overview

--- app/src/main/java/com/example/genshinapp/Mondstadt2Fragment.java
+++ app/src/main/java/com/example/genshinapp/Mondstadt2Fragment.java
@@ -54,7 +54,7 @@ public class Mondstadt2Fragment extends Fragment {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(Mondstadt2Fragment.this)
-                        .navigate(R.id.action_Mondstadt_to_eulaFragment);
+                        .navigate(R.id.action_mondstadt2_to_bennettFragment);
             }
         });
 
