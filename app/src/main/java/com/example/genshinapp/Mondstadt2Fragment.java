055e5caeecb93c81a274b5f6de27dd4bb2d629ee
William Moses <w.moses1@newcastle.ac.uk>
2021-06-10 11:41:43 +0100

    Fixed Razor Button in Mondstadt2Fragment.java

--- app/src/main/java/com/example/genshinapp/Mondstadt2Fragment.java
+++ app/src/main/java/com/example/genshinapp/Mondstadt2Fragment.java
@@ -45,7 +45,7 @@ public class Mondstadt2Fragment extends Fragment {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(Mondstadt2Fragment.this)
-                        .navigate(R.id.action_Mondstadt_to_lisaFragment);
+                        .navigate(R.id.action_mondstadt2_to_razorFragment);
             }
         });
 
